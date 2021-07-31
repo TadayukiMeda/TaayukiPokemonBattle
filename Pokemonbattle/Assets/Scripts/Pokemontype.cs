aee63f7edb5aae05a34cc7b287f0b9d35a852ad8
TadayukiMeda <88228330+TadayukiMeda@users.noreply.github.com>
2021-07-31 18:28:21 +0900

    Pokemon
    Add:既存のUnityプロジェクトをリポジトリに追加

---
+++ Pokemonbattle/Assets/Scripts/Pokemontype.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Type", menuName = "ScriptableObjects/PokemonType")]
+public class Pokemontype : ScriptableObject
+{
+    public　Ptype Mytype;
+    
+
+    [SerializeField,Header("苦手なタイプ")] List<Ptype> nigate;
+         
+}
+
